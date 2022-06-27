ccc943e95de9e1de00c4dcd11f78b5ea2c101c77
Konstabel Piksel <74968137+konstabelpiksel@users.noreply.github.com>
2022-06-27 09:36:07 +0800

    Update dashboard.js

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -2,24 +2,33 @@
 import React, { useState } from "react";
 import { getSession, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { signOut } from 'next-auth/react'
+import Link from 'next/link';
+
+//layout
+import Layout from "@/components/Layout";
 
 //bootstrap
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, ListGroup, ListGroupItem, Badge } from 'reactstrap';
 
 //toast
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+//dashboard services
+import { APIfetch } from "@/lib/APIservices";
 
 export async function getServerSideProps(ctx) {
 
     const session = await getSession(ctx);
 
     //check if session exist
-    if (session) {
+    if (session && session.user.email) {
+
+        //if it does, get user roles in subsystems    
+        const roles = await APIfetch.getRoles(session.user.email);
         return {
             props: {
-                what: what
+                userRoles: JSON.stringify(roles)
             }
         }
     }
@@ -30,38 +39,31 @@ export async function getServerSideProps(ctx) {
     }
 }
 
-export default function UserDashboard() {
+export default function UserDashboard(props) {
 
     const { data: session, status } = useSession();
     const loading = status === "loading";
     const router = useRouter();
 
     return (
-        <Container>
+        <Layout session={session}>
+            <Container fluid className="px-4 py-2 w-100">
 
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={true}
-            />
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={true}
+                />
 
-            <h2>Dashboard</h2>
-            <Row>
-                <Col>
-                    Dashboard
-                </Col>
-                <a
-                    href={`/api/auth/signout`}
-                    onClick={(e) => {
-                        e.preventDefault()
-                        signOut({
-                            callbackUrl: `/`
-                        })
-                    }}
-                >LOGOUT
-                </a>
-            </Row>
-        </Container>
+                <h2>Dashboard</h2>
+                <Row>
+                    <Col>
+                        <h1>Hello Dashboard</h1>
+                    </Col>
+                </Row>
+
+            </Container>
+        </Layout>
 
     );
 };
