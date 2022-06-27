fcb4d475350b6d3cfe75c44fc47413aa9aaf51d5
Konstabel Piksel <74968137+konstabelpiksel@users.noreply.github.com>
2022-06-27 10:10:34 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,7 +11,7 @@ export default function SystemNavbar({ dashsidebar, setDashsidebar, session }){
     return (
         <Navbar className={styles.dashnavbar}>
             <a href="#" onClick={() => setDashsidebar(!dashsidebar)}>
-                <Image src="/sso/images/hamburger.svg" width="30" height="30" alt=""/>
+                <Image src="/images/hamburger.svg" width="30" height="30" alt=""/>
             </a>
             <div>
                 <h4>{session? session.user.username : 'No session'} / {session? session.user.email : 'No role'} </h4>
